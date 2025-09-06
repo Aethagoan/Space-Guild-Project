f035514e715bcbdbcfa050685b2406cf43ffcbff
aethagoan <aethagoan@outlook.com>
2025-09-05 21:58:28 -0600

    create shipcomponent classes and ship class first drafts

---
+++ SpaceGuild.Tests/CargoTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SpaceGuild {
+    public class CargoTests {
+        [Fact]
+        public void TestCargoAddItem() {
+            Cargo cargo = new Cargo();
+            Assert.True(cargo.AddItem("Item1", 10));
+            Assert.False(cargo.AddItem("Item2", 20));
+            Assert.False(cargo.AddItem("Item3", 30));
+        }
+    }
+}
